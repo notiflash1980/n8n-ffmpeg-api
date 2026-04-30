1c26310c18c380531c5072744bf26d365e384096
notiflash1980 <notiflash1980@gmail.com>
2026-04-30 22:16:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,27 @@ app = Flask(__name__)
 def render():
     data = request.json
 
-    # guardar JSON recibido
+    # guardar JSON
     with open("data.json", "w") as f:
         json.dump(data, f)
 
-    print("JSON guardado correctamente")
+    images = data.get("images", [])
+    duration = data.get("duration", 3)
+
+    # generar list.txt
+    with open("list.txt", "w") as f:
+        for img in images:
+            f.write(f"file '{img}'\n")
+            f.write(f"duration {duration}\n")
+
+        if images:
+            f.write(f"file '{images[-1]}'\n")
+
+    print("list.txt generado")
 
     return jsonify({
         "status": "ok",
-        "message": "json guardado"
+        "message": "list.txt generado"
     })
 
 if __name__ == "__main__":
